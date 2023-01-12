7dc6bd42f6a861773bfa0b352dbc769d4f846295
cormac21 <gaio.lucas0@gmail.com>
2023-01-12 11:05:48 -0300

    Initial commit

---
+++ src/main/java/com/cormac/greenphoenix/GreenPhoenixApplication.java
@@ -0,0 +1,13 @@
+package com.cormac.greenphoenix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenPhoenixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenPhoenixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cormac/greenphoenix/GreenPhoenixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cormac.greenphoenix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenPhoenixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
